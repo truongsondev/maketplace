728ad56f3696eed24f24adf2e41ca44690e7ffe7
truongson <sonltute@gmail.com>
2026-04-02 12:47:04 +0700

    feat: config docker for production

--- client-next/next.config.js
+++ client-next/next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   turbopack: {},
   webpack: (config, { dev }) => {
     if (dev) {
@@ -26,4 +25,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
